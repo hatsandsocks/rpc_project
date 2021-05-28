c19e307ae3090de45c469a82695b693687d8d367
Tanner Croshaw <tannercroshaw@hotmail.com>
2021-05-27 17:06:07 -0700

    building random number generator

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function computerPlay() {
+    function getRandomInt(1,4) {
+        min = Math.ceil(1);
+        max = Math.floor(4);
+        return Math.floor(Math.ramom() * (max-min) + min);
+    }
+}
+
+
 function rockFunction() {
     alert("rock")
 }

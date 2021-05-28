eb3fbb3e58f5cefa0ff790dfce10632aa95ee1dc
Tanner Croshaw <tannercroshaw@hotmail.com>
2021-05-27 18:22:42 -0700

    random # working

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function computerPlay() {
-    function getRandomInt(1,4) {
-        min = Math.ceil(1);
-        max = Math.floor(4);
-        return Math.floor(Math.ramom() * (max-min) + min);
-    }
-}
+
+
+
 
 
 function rockFunction() {
-    alert("rock")
+    alert("rock");
 }
 function paperFunction() {
-    alert("paper")
+    alert("paper");
 }
 function scissorsFunction() {
-    alert("scissors")
+    alert("scissors");
+}
+
+
+function computerPlay() {
+    var randomNumber = Math.floor((Math.random()*3)+1);
+    alert(randomNumber);
 }
